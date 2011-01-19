186e10f07267303e087f0e2b30771ecb0257e4a2
voineali <voineali@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-01-19 15:47:52 +0000

    - Hudson fix: Fix tests,

--- trunk/eea.translations/eea/translations/tests.py
+++ trunk/eea.translations/eea/translations/tests.py
@@ -1,3 +1,5 @@
+""" Tests
+"""
 import unittest
 
 from zope.testing import doctestunit
@@ -13,9 +15,15 @@ ptc.setupPloneSite()
 import eea.translations
 
 class TestCase(ptc.PloneTestCase):
+    """ Test case
+    """
     class layer(PloneSite):
+        """ Layer
+        """
         @classmethod
         def setUp(cls):
+            """ Setup
+            """
             fiveconfigure.debug_mode = True
             zcml.load_config('configure.zcml',
                              eea.translations)
@@ -23,31 +31,34 @@ class TestCase(ptc.PloneTestCase):
 
         @classmethod
         def tearDown(cls):
+            """ Setup
+            """
             pass
-    
+
 
 def test_suite():
+    """ Suite
+    """
     return unittest.TestSuite([
 
         # Unit tests
         #doctestunit.DocFileSuite(
-        #    'README.txt', package='eea.translations',
-        #    setUp=testing.setUp, tearDown=testing.tearDown),
+            #'README.txt', package='eea.translations',
+            #setUp=testing.setUp, tearDown=testing.tearDown),
 
         #doctestunit.DocTestSuite(
-        #    module='eea.translations.mymodule',
-        #    setUp=testing.setUp, tearDown=testing.tearDown),
-
+            #module='eea.translations.mymodule',
+            #setUp=testing.setUp, tearDown=testing.tearDown),
 
         # Integration tests that use PloneTestCase
-        #ztc.ZopeDocFileSuite(
-        #    'README.txt', package='eea.translations',
-        #    test_class=TestCase),
+        ztc.ZopeDocFileSuite(
+            'README.txt', package='eea.translations',
+            test_class=TestCase),
 
         #ztc.FunctionalDocFileSuite(
-        #    'browser.txt', package='eea.translations',
-        #    test_class=TestCase),
-        
+            #'browser.txt', package='eea.translations',
+            #test_class=TestCase),
+
         ])
 
 if __name__ == '__main__':
