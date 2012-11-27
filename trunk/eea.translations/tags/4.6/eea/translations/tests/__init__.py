80e6cbb3f0043b8089514cef51040f87da5ff6fa
ichimdav <ichimdav@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-11-27 13:07:13 +0000

    Tagged eea.translations 4.6.

---
+++ trunk/eea.translations/tags/4.6/eea/translations/tests/__init__.py
@@ -0,0 +1,10 @@
+""" tests
+"""
+import unittest
+
+#TODO: dummy test_suite as for yet unknown reasons zope.testing
+#      instrospection is looking for a test suite in this module
+def test_suite():
+    """ Suite
+    """
+    return unittest.TestSuite(())
