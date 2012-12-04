f7e2750c095bcc2253af9f77b8505c44e3d42ec7
prospchr <prospchr@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-12-04 15:54:02 +0000

    Bug fix: Removed TODO statement to make jenkins happy

--- eea/translations/tests/__init__.py
+++ eea/translations/tests/__init__.py
@@ -2,8 +2,8 @@
 """
 import unittest
 
-#TODO: dummy test_suite as for yet unknown reasons zope.testing
-#      instrospection is looking for a test suite in this module
+# dummy test_suite as for yet unknown reasons zope.testing
+# instrospection is looking for a test suite in this module
 def test_suite():
     """ Suite
     """
