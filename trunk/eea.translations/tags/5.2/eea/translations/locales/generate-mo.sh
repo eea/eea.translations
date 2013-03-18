e4575a3a72f3f9c4bd7c6cbb1e9a5c295c957843
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2013-03-18 09:17:09 +0000

    Tagged eea.translations 5.2.

---
+++ trunk/eea.translations/tags/5.2/eea/translations/locales/generate-mo.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# List of supported languages for UI
+LANGUAGES="bg da el es fi hu it lt mt no pt ru sl tr cs de et fr is kl lv nl pl ro sk sv"
+
+# build the mo files
+for LANG in $LANGUAGES; do
+
+  PM="$LANG/LC_MESSAGES"
+
+  # Generate mo file
+  msgfmt -o $PM/eea.mo $PM/eea.po
+
+done
+
