dd8d7dd92a892b23bee56474e279ffdd795dd659
prospchr <prospchr@24821446-e7ea-0310-81ac-b189f4507b0b>
2013-05-28 14:20:22 +0000

    Updated script to generate mo files.

--- trunk/eea.translations/trunk/eea/translations/locales/generate-mo.sh
+++ trunk/eea.translations/trunk/eea/translations/locales/generate-mo.sh
@@ -1,15 +1,8 @@
 #!/bin/sh
 
-# List of supported languages for UI
-LANGUAGES="bg da el es fi hu it lt mt no pt ru sl tr cs de et fr is kl lv nl pl ro sk sv"
-
-# build the mo files
-for LANG in $LANGUAGES; do
-
-  PM="$LANG/LC_MESSAGES"
-
-  # Generate mo file
-  msgfmt -o $PM/eea.mo $PM/eea.po
-
+# Generate mo file from any po files in any folders
+for PO in $(find */LC_MESSAGES/ -name "*.po"); do
+  msgfmt -o ${PO/%.po/.mo} $PO;
 done
+
 
